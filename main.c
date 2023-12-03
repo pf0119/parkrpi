12009f7460afcfa5739b1e93edba71744f242815
jwpark <pf0119@gmail.com>
2023-12-03 23:48:34 +0900

    Add 3. Signal code in input.c, mian.c (most reference code)

--- main.c
+++ main.c
@@ -6,12 +6,53 @@
 #include <input.h>
 #include <web_server.h>
 
+/* 3. 시그널 */
+static void signalHandler(int sig)
+{
+    int status, savedErrno;
+    pid_t childPid;
+
+    savedErrno = errno;
+
+    printf("signalHandler param sig : %d\n", sig);
+
+    while ((childPid = waitpid(-1, &status, WNOHANG)) > 0) {
+        printf("handler: Reaped child %ld - ", (long) childPid);
+        //(NULL, status);
+    }
+
+    if (childPid == -1 && errno != ECHILD)
+        printf("waitpid");
+
+    printf("signalHandler now ret\n");
+
+    errno = savedErrno;
+
+    return;
+}
+
 int main()
 {
+    /* 2. 프로세스 관련 시스템 콜 */
     pid_t spid, gpid, ipid, wpid;
     int status, savedErrno;
 
     (void)savedErrno;
+
+    /* 3. 시그널 */
+    int sigCnt;
+
+    (void)sigCnt;
+
+    if (signal(SIGCHLD, signalHandler) == SIG_ERR)
+    {
+        perror("Could not signal user signal");
+        abort();
+
+        return 0;
+    }
+
+    /* 2. 프로세스 관련 시스템 콜 */
     printf("메인 함수입니다.\n");
     printf("시스템 서버를 생성합니다.\n");
     spid = create_system_server();
--- ui/input.c
+++ ui/input.c
@@ -6,9 +6,84 @@
 #include <input.h>
 #include <web_server.h>
 
+#include <assert.h>
+#include <pthread.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <ucontext.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* 3. 시그널 */
+typedef struct _sig_ucontext {
+    unsigned long uc_flags;
+    struct ucontext *uc_link;
+    stack_t uc_stack;
+    struct sigcontext uc_mcontext;
+    sigset_t uc_sigmask;
+} sig_ucontext_t;
+
+// 레퍼런스 코드
+void segfault_handler(int sig_num, siginfo_t * info, void * ucontext) {
+  void * array[50];
+  void * caller_address;
+  char ** messages;
+  int size, i;
+  sig_ucontext_t * uc;
+
+  uc = (sig_ucontext_t *) ucontext;
+
+  /* Get the address at the time the signal was raised */
+  caller_address = (void *) uc->uc_mcontext.rip;  // RIP: x86_64 specific     arm_pc: ARM
+
+  fprintf(stderr, "\n");
+
+  if (sig_num == SIGSEGV)
+    printf("signal %d (%s), address is %p from %p\n", sig_num, strsignal(sig_num), info->si_addr,
+           (void *) caller_address);
+  else
+    printf("signal %d (%s)\n", sig_num, strsignal(sig_num));
+
+  size = backtrace(array, 50);
+  /* overwrite sigaction with caller's address */
+  array[1] = caller_address;
+  messages = backtrace_symbols(array, size);
+
+  /* skip first stack frame (points here) */
+  for (i = 1; i < size && messages != NULL; ++i) {
+    printf("[bt]: (%d) %s\n", i, messages[i]);
+  }
+
+  free(messages);
+
+  exit(EXIT_FAILURE);
+}
+
 int input()
 {
+    /* 3. 시그널 */
+    int retcode;
+    struct sigaction sa;
+
+    (void)retcode;
+
     printf("나 input 프로세스!\n");
+
+    memset(&sa, 0, sizeof(sigaction));
+    sigemptyset(&sa.sa_mask);
+
+    sa.sa_flags = SA_RESTART | SA_SIGINFO;
+    sa.sa_sigaction = segfault_handler;
+
+    if(sigaction(SIGSEGV, &sa, NULL) < 0)
+    {
+        printf("sigaction failed!!\n");
+
+        return 0;
+    }
 
     while (1) {
         sleep(1);
@@ -19,6 +94,7 @@ int input()
 
 int create_input()
 {
+    /* 2. 프로세스 관련 시스템 콜 */
     pid_t systemPid;
     const char *name = "input";
 
