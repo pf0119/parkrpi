13a6b826d483ceed8240168d0856d5d5b457aa36
jwpark <pf0119@gmail.com>
2023-12-26 23:36:07 +0900

    Add pid isolation to web server

--- web_server/web_server.c
+++ web_server/web_server.c
@@ -1,30 +1,69 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sched.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/mman.h>
 
 #include <system_server.h>
 #include <gui.h>
 #include <input.h>
 #include <web_server.h>
 
+#define STACK_SIZE (8 * 1024 * 1024)
+
+static int child_func()
+{
+    char path[1024];
+
+    if(getcwd(path, 1024) == NULL)
+    {
+        fprintf(stderr, "current working directory get error: %s\n", strerror(errno));
+        return -1;
+    }
+
+    printf(" - [%4d] Current namspace, Parent PID : %d\n", getpid(), getppid() );
+    printf("current working directory: %s\n", path);
+
+    // 파일 시스템 완전 격리를 위해서는 pivot_root 필요
+
+    if(execl("/usr/local/bin/filebrowser", "filebrowser", "-p", "8080", (char *)NULL))
+        printf("execfailed\n");
+
+    return 0;
+}
+
 int create_web_server()
 {
-    pid_t systemPid;
+    pid_t child_pid;
 
     printf("여기서 Web Server 프로세스를 생성합니다.\n");
 
-    systemPid = fork();
-    if(!systemPid)
+    /* 6.6.2. 컨테이너 관련 시스템 콜 */
+    char* stack = mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0);
+
+    if(stack == MAP_FAILED)
     {
-        if(execl("/usr/local/bin/filebrowser", "filebrowser", "-p", "8080", (char *)NULL))
-            printf("execfailed\n");
+        printf("mmap failed\n");
+        return -1;
     }
-    else if(systemPid > 0)
+
+    child_pid = clone(child_func, stack + STACK_SIZE, CLONE_NEWUTS | CLONE_NEWIPC | CLONE_NEWPID | CLONE_NEWNS | SIGCHLD, "Hello");
+
+    if(child_pid == -1)
     {
-        ;
+        perror("clone\n");
+        exit(EXIT_FAILURE);
     }
-    else
-    {
-        printf("fork() failed!!\n");
-    }
+
+    printf("PID of child created by clone() is %ld\n", (long)child_pid);
+
+    munmap(stack, STACK_SIZE);
 
     return 0;
 }
